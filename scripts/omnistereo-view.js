12a58fe699a59f69a145a45015de11d9bf194109
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2020-08-06 21:31:02 -0500

    remove width/height constraint

--- scripts/omnistereo-view.js
+++ scripts/omnistereo-view.js
@@ -1,16 +1,16 @@
 function init() {
     let vrview_set1_a = new VRView.Player('#vrview_set1_a', {
         image: 'images/pointcloud_5M_4096_a.jpg',
-        width: 360,
-        height: 270,
+        //width: 360,
+        //height: 270,
         default_yaw: 270,
         is_stereo: true
     });
 
     let vrview_set2_a = new VRView.Player('#vrview_set2_a', {
         image: 'images/obj_3d_scene_4096_a.jpg',
-        width: 360,
-        height: 270,
+        //width: 360,
+        //height: 270,
         default_yaw: 90,
         is_stereo: true
     });
