6cb820bcb8fc843debc3843ffe59845380d37534
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2020-08-06 21:25:31 -0500

    add power-of-2 images

--- scripts/omnistereo-view.js
+++ scripts/omnistereo-view.js
@@ -1,6 +1,6 @@
 function init() {
     let vrview_set1_a = new VRView.Player('#vrview_set1_a', {
-        image: 'images/pointcloud_5M_a.jpg',
+        image: 'images/pointcloud_5M_4096_a.jpg',
         width: 360,
         height: 270,
         default_yaw: 270,
@@ -8,7 +8,7 @@ function init() {
     });
 
     let vrview_set2_a = new VRView.Player('#vrview_set2_a', {
-        image: 'images/obj_3d_scene_a.jpg',
+        image: 'images/obj_3d_scene_4096_a.jpg',
         width: 360,
         height: 270,
         default_yaw: 90,
